5783c730bb59fbf5f9bb11c2c246ef9048b82b63
unknown <otabekshavkatov956@gmail.com>
2024-11-22 03:50:37 +0500

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    './src/**/*.{html,js}', // src papkadagi barcha fayllar
-    './dist/**/*.{html,js}', // dist papkadagi barcha fayllar (builddan keyin)
+    './**/*.{html,js}', // Hamma HTML va JS fayllar
+    './src/**/*.{html,js}', // SRC papkasi fayllari
+    './dist/**/*.{html,js}', // DIST papkasi fayllari
   ],
   plugins: [require('preline/plugin')],
   theme: {
