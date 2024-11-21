4b7229c730a649e1083c162c43c726d7f6a90db8
unknown <otabekshavkatov956@gmail.com>
2024-11-22 00:49:31 +0500

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
   ],
   plugins: [require('preline/plugin')],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        heartbeat: 'heartbeat 1.5s ease-in-out infinite',
+      },
+      keyframes: {
+        heartbeat: {
+          '0%, 100%': {
+            transform: 'scale(1)',
+          },
+          '50%': {
+            transform: 'scale(1.2)',
+          },
+        },
+      },
+    },
   },
 };
