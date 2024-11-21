36c247e905cca05eb587c8dce031a978ff54b8d2
unknown <otabekshavkatov956@gmail.com>
2024-11-22 01:38:37 +0500

    fix path src \ dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,11 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    './src/**/*.{html,js}', // Barcha HTML va JS fayllar faqat src ichida
+    './src/**/*.{html,js}', // src papkadagi barcha fayllar
+    './dist/**/*.{html,js}', // dist papkadagi barcha fayllar (builddan keyin)
   ],
   plugins: [require('preline/plugin')],
   theme: {
-    extend: {
-      animation: {
-        heartbeat: 'heartbeat 1.5s ease-in-out infinite',
-      },
-      keyframes: {
-        heartbeat: {
-          '0%, 100%': {
-            transform: 'scale(1)',
-          },
-          '50%': {
-            transform: 'scale(1.2)',
-          },
-        },
-      },
-    },
+    extend: {},
   },
 };
